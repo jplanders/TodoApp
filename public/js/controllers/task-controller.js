6f5c8e95240b4ac720d0bdbcc7554a9ac9b4a6ba
Jon Landers <decalaide@gmail.com>
2015-10-03 17:26:17 -0500

    body-parser added new tasks sent to server
    new tasks are now sent to the server to be stored in the DB

--- public/js/controllers/task-controller.js
+++ public/js/controllers/task-controller.js
@@ -1,6 +1,15 @@
 todoApp.controller('taskController', ['$scope', '$resource', function ($scope, $resource) {
 // scope is used to talk between view and controller
-	var Todo = $resource('/api/todos')
+	var Todo = $resource('/api/tasklist')
+
+	function getCurDate () {
+		var longDate = new Date();
+		console.log(longDate);
+		console.log(longDate.getMonth());
+		console.log(longDate.getDay());
+		console.log(longDate.getFullYear());
+		return (longDate.getMonth()+1) + "/" + longDate.getDate() + "/" + longDate.getFullYear();
+	};
 
 	$scope.tasklist = [
 		{	name: "Walk the dog",
@@ -16,4 +25,16 @@ todoApp.controller('taskController', ['$scope', '$resource', function ($scope, $
 			startDate: "10/2/2015",
 			endDate: "10/3/2015"}
 	]
+
+	$scope.createTask = function () {
+		//$scope.tasklist.push({ name: $scope.taskName, startDate: getCurDate(), endDate: "" });
+		//$scope.taskName = "";
+		var todo = new Todo();
+		todo.name = $scope.taskName;
+		todo.startDate = getCurDate();
+		todo.endDate = "";
+		todo.$save();
+		$scope.taskName = "";
+
+	}
 }]);
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-var express = require('express'),
-	app		= express();
+var express 		= require('express'),
+	app				= express();
+	bodyParser 		= require('body-parser')
+	taskController 	= require('./server/controllers/task-controller')
+
+app.use(bodyParser());
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/public/view/index.html');
 });
 
 app.use('/js', express.static(__dirname + '/public/js'));
+
+app.post('/api/tasklist', taskController.create);
 
 app.listen(3000, function() {
 	console.log('The server is listening...');
